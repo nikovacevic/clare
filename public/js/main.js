0fc28d04c8c02545b6a2b81466ace4de7ee7d3db
Niko Kovacevic <nikovacevic@gmail.com>
2018-05-23 10:42:11 -0600

    Full jQuery, smooth scrolling, and updated images

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ var Clare = {};
 $(function () {
   UI = {};
 
+  // Header nav background
   UI.setNavScroll = function (nav, limit) {
     nav.toggleClass('scrolled', $(document).scrollTop() > limit);
   }
@@ -10,11 +11,28 @@ $(function () {
     UI.setNavScroll(nav, 100);
   });
 
+  // Full-screen nav toggle
   UI.toggleFullScreenNav = function (nav) {
     nav.toggleClass('visible');
+    if (nav.hasClass('visible')) {
+      $("html, body").addClass('no-scroll');
+    } else {
+      $("html, body").removeClass('no-scroll');
+    }
   }
   $('.toggle-menu').click(function () {
     var nav = $("nav#full-screen-nav");
     UI.toggleFullScreenNav(nav);
+  });
+
+  // Scroll to anchor
+  $(document).ready(function () {
+    $("a.smooth-scroll").click(function (e) {
+      e.preventDefault();
+      if ($("nav#full-screen-nav").hasClass('visible')) {
+        UI.toggleFullScreenNav($("nav#full-screen-nav"));
+      }
+      $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top - 58}, 400);
+    });
   });
 });
